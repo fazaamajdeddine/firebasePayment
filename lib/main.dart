06f9f0f25531a41ac0abc6ae079ded288d3fe6e6
majdeddine fazaa <fazaamajdfazaa@gmail.com>
2022-09-22 15:46:28 +0100

    payment screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebasepayment/payment_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase Payment',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const PaymenScreen(),
+    );
+  }
+}
